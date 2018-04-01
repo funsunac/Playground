c455c271caf47c12083bc90a84ebbafafada95d9
STommydx <ttoo74@gmail.com>
2018-04-01 20:24:24 +0800

    Initial commit

---
+++ app/src/main/java/com/company/g1/a1g1_madp/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.company.g1.a1g1_madp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+}
