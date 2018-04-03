49c72c97a63a072386d5744f39c8bbf8fab8c6ba
Jeanie <clr715@gmail.com>
2018-04-03 15:27:07 +0800

    chmod for cirleci

--- app/src/androidTest/java/com/company/g1/a1g1_madp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/company/g1/a1g1_madp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.company.g1.g1extrateamlab;
+package com.company.g1.a1g1_madp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 	@Test
-	public void useAppContext() throws Exception {
+	public void useAppContext() {
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("com.company.g1.g1extrateamlab", appContext.getPackageName());
+		assertEquals("com.company.g1.a1g1_madp", appContext.getPackageName());
 	}
 }
--- app/src/test/java/com/company/g1/a1g1_madp/ExampleUnitTest.java
+++ app/src/test/java/com/company/g1/a1g1_madp/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.company.g1.g1extrateamlab;
+package com.company.g1.a1g1_madp;
 
 import org.junit.Test;
 
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
 	@Test
-	public void addition_isCorrect() throws Exception {
+	public void addition_isCorrect() {
 		assertEquals(4, 2 + 2);
 	}
 }
